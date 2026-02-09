a3c6c67aa5ae7f21d04f4cdb05538981223e5008
marinarj <marinarj@myumanitoba.ca>
2026-02-09 16:14:19 -0600

    Replace paragraph tags with headings

--- web/src/components/GameConfiguration.vue
+++ web/src/components/GameConfiguration.vue
@@ -1,18 +1,18 @@
 <template>
     <v-container class="fill-height d-flex align-center justify-center">
         <v-sheet height="520" width="520" class="pa-8 d-flex flex-column" rounded="xl" elevation="6">
-            <p class="text-center text-h1 font-weight-bold">
+            <h1 class="text-center text-h1 font-weight-bold">
                 UMG!
-            </p>
-            <p class="text-center text-h4">
+            </h1>
+            <h2 class="text-center text-h4">
                 UManitobaGuessr
-            </p>
+            </h2>
 
             <v-spacer />
 
-            <p class="text-center text-h6">
+            <h3 class="text-center text-h6">
                 Rounds
-            </p>
+            </h3>
             <v-btn-toggle v-model="rounds" variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
                 <v-btn :value="5">5</v-btn>
                 <v-btn :value="10">10</v-btn>
@@ -20,9 +20,9 @@
                 <v-btn :value="20">20</v-btn>
             </v-btn-toggle>
 
-            <p class="text-center text-h6 mt-8">
+            <h3 class="text-center text-h6 mt-8">
                 Timer
-            </p>
+            </h3>
             <v-btn-toggle v-model="timer" variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
                 <v-btn :value="0">Off</v-btn>
                 <v-btn :value="30">30 sec</v-btn>
