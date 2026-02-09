f2f5df8933b96530a8bbd21ce5a5a7b8d16d80a2
marinarj <marinarj@myumanitoba.ca>
2026-02-09 13:09:11 -0600

    Add centered game config sheet

--- web/src/components/GameConfiguration.vue
+++ web/src/components/GameConfiguration.vue
@@ -1,5 +1,9 @@
 <template>
-    <v-btn color="primary">Start Game</v-btn>
+    <v-container class="fill-height d-flex align-center justify-center">
+        <v-sheet height="520"width="520" class="pa-8 d-flex flex-column" rounded="xl" elevation="6">
+            <v-btn color="primary">Start Game</v-btn>
+        </v-sheet>
+    </v-container>
 </template>
 
 <script lang="ts" setup>
