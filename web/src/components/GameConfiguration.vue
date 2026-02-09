07be585c99547b6b38484e216614c701ad3d642d
marinarj <marinarj@myumanitoba.ca>
2026-02-09 15:47:56 -0600

    Add state to toggle buttons

--- web/src/components/GameConfiguration.vue
+++ web/src/components/GameConfiguration.vue
@@ -7,26 +7,28 @@
             <p class="text-center text-h4">
                 UManitobaGuessr
             </p>
+
             <v-spacer />
+
             <p class="text-center text-h6">
                 Rounds
             </p>
-            <v-btn-toggle variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
-                <v-btn>5</v-btn>
-                <v-btn>10</v-btn>
-                <v-btn>15</v-btn>
-                <v-btn>20</v-btn>
+            <v-btn-toggle v-model="rounds" variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
+                <v-btn :value="5">5</v-btn>
+                <v-btn :value="10">10</v-btn>
+                <v-btn :value="15">15</v-btn>
+                <v-btn :value="20">20</v-btn>
             </v-btn-toggle>
 
             <p class="text-center text-h6 mt-8">
                 Timer
             </p>
-            <v-btn-toggle variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
-                <v-btn>Off</v-btn>
-                <v-btn>30 sec</v-btn>
-                <v-btn>1 min</v-btn>
-                <v-btn>5 min</v-btn>
-                <v-btn>10 min</v-btn>
+            <v-btn-toggle v-model="timer" variant="outlined" class="flex-toggle-btn justify-center w-100 mt-4" mandatory divided>
+                <v-btn :value="0">Off</v-btn>
+                <v-btn :value="30">30 sec</v-btn>
+                <v-btn :value="60">1 min</v-btn>
+                <v-btn :value="300">5 min</v-btn>
+                <v-btn :value="600">10 min</v-btn>
             </v-btn-toggle>
 
             <v-spacer/>
@@ -37,7 +39,8 @@
 </template>
 
 <script lang="ts" setup>
-  //
+const rounds = ref<number>(5);
+const timer = ref<number>(0);
 </script>
 
 <style scoped>
