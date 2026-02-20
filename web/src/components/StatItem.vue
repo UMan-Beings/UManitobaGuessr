7199d1124155ecf4649991bcbff52fa77626ec02
marinarj <marinarj@myumanitoba.ca>
2026-02-20 16:29:32 -0600

    Add game statistic components

---
+++ web/src/components/StatItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex flex-column">
+    <dt class="text-caption text-medium-emphasis">
+      {{ label }}
+    </dt>
+    <dd class="text-h5 font-weight-bold">
+      {{ value }}
+    </dd>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineProps<{
+    label: string
+    value: string
+  }>()
+</script>
