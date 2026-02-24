fb9fbbfaf12fba38421d40d5be9638a9c356e6b5
marinarj <marinarj@myumanitoba.ca>
2026-02-24 17:57:31 -0600

    Add guess result component

---
+++ web/src/components/GuessResult.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-sheet class="rounded-lg blur pl-6 pr-6 pt-4 pb-4 w-auto">
+    <StatItem class="text-right" label="+ Score" :value="score" />
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    score: number
+  }>()
+</script>
+
+<style scoped>
+.blur{
+  backdrop-filter: blur(6px);
+  background: rgba(0,0,0,.25);
+}
+</style>
