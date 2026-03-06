4f8d5669e3a5f51462d7d1b743600c1dc02ed12f
marinarj <marinarj@myumanitoba.ca>
2026-03-05 19:15:59 -0600

    Connect game pages to backend API

--- web/src/pages/game/[gameId].vue
+++ web/src/pages/game/[gameId].vue
@@ -1,165 +1,210 @@
 <template>
   <HomeLinkLogo class="game-overlay position-absolute top-0 left-0 ma-4" />
 
-  <div v-if="phase!=='FINISHED'">
+  <div v-if="loading" class="h-screen w-100 d-flex flex-column align-center justify-center">
+    Loading...
+  </div>
+
+  <div v-else-if="error !== null" class="h-screen w-100 d-flex flex-column align-center justify-center">
+    {{ error }}
+  </div>
+
+  <div v-else-if="phase === 'GUESS' || phase === 'REVEAL'">
     <div class="game-overlay position-absolute top-0 right-0 ma-4 d-flex flex-column align-end">
-      <GameStats :round="round" :score="score" :time-seconds="timer" :total-rounds="totalRounds" />
-      <GuessResult v-if="phase==='REVEAL'" class="mt-2 position-relative bottom-0 right-0" :score="scoreReceived" />
+      <GameStats
+        :round="round"
+        :score="score"
+        :time-seconds="timer"
+        :total-rounds="totalRounds"
+      />
+      <GuessResult
+        v-if="phase === 'REVEAL'"
+        class="mt-2 position-relative bottom-0 right-0"
+        :score="scoreReceived"
+      />
+    </div>
+
+    <div v-if="phase === 'GUESS'">
+      <v-img
+        class="h-screen w-auto"
+        :src="imageUrl"
+      />
+      <GuessMap
+        class="position-absolute bottom-0 right-0 ma-4"
+        @guess="submitGuess"
+      />
     </div>
 
     <RevealMap
-      v-if="phase ==='REVEAL'"
+      v-else-if="phase ==='REVEAL'"
       :actual-lat="actualLat"
       :actual-lng="actualLng"
       :guess-lat="guessLat"
       :guess-lng="guessLng"
       @next="nextRound"
     />
-
-    <div v-if="phase==='GUESS'">
-      <v-img
-        class="h-screen w-auto"
-        :src="imageUrl"
-      />
-      <GuessMap class="position-absolute bottom-0 right-0 ma-4" @guess="submitGuess" />
-    </div>
   </div>
-  <div v-else class="h-screen w-100 d-flex flex-column align-center justify-center">
+
+  <div v-else-if="phase === 'FINISHED'" class="h-screen w-100 d-flex flex-column align-center justify-center">
     <StatItem class="text-center" label="Score" :value="String(score)" />
+  </div>
+
+  <div v-else class="h-screen w-100 d-flex flex-column align-center justify-center">
+    Unknown phase ({{ phase }})
   </div>
 </template>
 
 <script lang="ts" setup>
-  // locationIndex & locations will be removed when the API is ready
-  // they are here to demo the UI
-  let locationIndex = 0
-  const locations = [
-    {
-      imageUrl: 'https://uman-beings.github.io/UMG-Pictures/crazy-balance-v0-kzeho9rnepre1.jpeg',
-      lat: 1340,
-      lng: 2282,
-    },
-    {
-      imageUrl: 'https://uman-beings.github.io/UMG-Pictures/deer1.jpeg',
-      lat: 833,
-      lng: 2705,
-    },
-    {
-      imageUrl: 'https://uman-beings.github.io/UMG-Pictures/IMG_3651.jpg',
-      lat: 1480,
-      lng: 1770,
-    },
-    {
-      imageUrl: 'https://uman-beings.github.io/UMG-Pictures/IMG_3652.jpg',
-      lat: 1172,
-      lng: 1776,
-    },
-    {
-      imageUrl: 'https://uman-beings.github.io/UMG-Pictures/IMG_3655.jpg',
-      lat: 1344,
-      lng: 1209,
-    },
-  ]
+  const gameId = useRoute('/game/[gameId]').params.gameId
 
-  const timeLimitSeconds = 30
-  const timer = ref(timeLimitSeconds)
+  const loading = ref(false)
+  const error = ref<string | null>(null)
 
-  const round = ref(locationIndex + 1)
-  const totalRounds = ref(locations.length)
-
+  // common game state refs
+  const phase = ref('')
+  const round = ref(0)
+  const totalRounds = ref(0)
   const score = ref(0)
+
+  let timerIntervalId: number | null = null
+  let timeLimitSeconds = 0
+  const timer = ref(0)
+
+  // guess refs
+  const imageUrl = ref<string | undefined>(undefined)
+
+  // reveal refs
+  const guessLat = ref<number | undefined>(undefined) // corY
+  const guessLng = ref<number | undefined>(undefined) // corX
+  const actualLat = ref(0)
+  const actualLng = ref(0)
   const scoreReceived = ref(0)
 
-  const imageUrl = ref(locations[locationIndex]?.imageUrl)
+  onMounted(async () => {
+    const { game, ...otherProperties } = history.state ?? {}
 
-  const guessLat = ref<number | undefined>(undefined)
-  const guessLng = ref<number | undefined>(undefined)
-  const actualLat = ref(1485)
-  const actualLng = ref(2130)
+    if (game) {
+      updateGameRefs(game)
+      history.replaceState(otherProperties, '')
+    } else {
+      await getGameState()
+    }
 
-  const phase = ref('GUESS')
-
-  let intervalId: number | null = null
-
-  onMounted(() => {
-    intervalId = setInterval(() => {
-      if (timeLimitSeconds > 0 && timer.value > 0 && phase.value === 'GUESS') {
-        timer.value--
+    timerIntervalId = setInterval(() => {
+      if (timerIntervalId !== null && (phase.value === 'FINISHED' || error.value !== null)) {
+        clearInterval(timerIntervalId)
       }
 
-      if (timer.value == 0) {
-        phase.value = 'REVEAL'
-        guessLat.value = undefined
-        guessLng.value = undefined
-        scoreReceived.value = 0
-      }
+      if (!loading.value && phase.value === 'GUESS') {
+        timer.value++
 
-      if (phase.value === 'FINISHED' && intervalId !== null) {
-        clearInterval(intervalId)
+        if (timeLimitSeconds > 0 && timer.value == timeLimitSeconds) {
+          timeout()
+        }
       }
     }, 1000)
   })
 
   onUnmounted(() => {
-    if (intervalId !== null) {
-      clearInterval(intervalId)
+    if (timerIntervalId !== null) {
+      clearInterval(timerIntervalId)
     }
   })
 
-  // will be removed when the API is ready
-  function calculateScore (lat1: number, lng1: number, lat2: number, lng2: number) {
-    let calculatedScore = 0
-
-    const distance = Math.hypot(
-      lat1 - lat2,
-      lng1 - lng2,
-    )
-
-    const fullScoreDistance = 50
-    const maxDistance = 350
-    const maxScore = 1000
-
-    if (distance <= fullScoreDistance) {
-      calculatedScore = maxScore
-    } else {
-      const scaledDistance = distance - fullScoreDistance
-      const scoringRange = maxDistance - fullScoreDistance
-
-      calculatedScore = Math.max(
-        0,
-        Math.round(maxScore * (1 - scaledDistance / scoringRange)),
-      )
-    }
-
-    return calculatedScore
-  }
-
-  function submitGuess (lat: number, lng: number) {
-    phase.value = 'REVEAL'
-
+  function updateGuessCoordinates (lat?: number, lng?: number) {
     guessLat.value = lat
     guessLng.value = lng
-    console.log(`latlng: [${lat}, ${lng}]`)
-
-    actualLat.value = locations[locationIndex]!.lat
-    actualLng.value = locations[locationIndex]!.lng
-
-    scoreReceived.value = calculateScore(guessLat.value, guessLng.value, actualLat.value, actualLng.value)
   }
 
-  function nextRound () {
-    locationIndex++
+  function updateGameRefs (data: any) {
+    phase.value = data.phase
+    score.value = data.score
+    round.value = data.round
+    totalRounds.value = data.totalRounds
+    timeLimitSeconds = data.timeLimitSeconds
 
-    if (locationIndex < locations.length) {
-      phase.value = 'GUESS'
-      timer.value = timeLimitSeconds
-      imageUrl.value = locations[locationIndex]!.imageUrl
-      round.value = locationIndex + 1
-    } else {
-      phase.value = 'FINISHED'
+    if (data.phase === 'GUESS') {
+      updateGuessCoordinates()
+      imageUrl.value = data.imageUrl
+      timer.value = 0
+    } else if (data.phase === 'REVEAL') {
+      updateGuessCoordinates(data.guessedY, data.guessedX)
+      actualLat.value = data.actualY
+      actualLng.value = data.actualX
+      scoreReceived.value = data.scoreReceived
+      timer.value = data.guessTimeSeconds
+    }
+  }
+
+  async function callApi (doFetch: () => Promise<Response>) {
+    if (loading.value) {
+      return
     }
 
-    score.value += scoreReceived.value
+    try {
+      loading.value = true
+      error.value = null
+
+      const response = await doFetch()
+
+      if (!response.ok) {
+        throw new Error(`Request failed (${response.status} ${response.statusText})`)
+      }
+
+      const data = await response.json()
+      updateGameRefs(data)
+    } catch (caughtError) {
+      error.value = caughtError instanceof Error ? caughtError.message : 'Unexpected error'
+    } finally {
+      loading.value = false
+    }
+  }
+
+  async function getGameState () {
+    await callApi(() =>
+      fetch(`/api/v1/games/${gameId}`, {
+        method: 'GET',
+        headers: { Accept: 'application/json' },
+      }),
+    )
+  }
+
+  async function submitGuess (lat: number, lng: number) {
+    console.log(`${imageUrl.value}`)
+    console.log(`lat/lng (y/x): [${lat}, ${lng}]`)
+
+    await callApi(() =>
+      fetch(`/api/v1/games/${gameId}/guess`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          corY: lat,
+          corX: lng,
+          guessTimeSeconds: timer.value,
+        }),
+      }),
+    )
+  }
+
+  async function timeout () {
+    await callApi(() =>
+      fetch(`/api/v1/games/${gameId}/timeout`, {
+        method: 'POST',
+        headers: { Accept: 'application/json' },
+      }),
+    )
+  }
+
+  async function nextRound () {
+    await callApi(() =>
+      fetch(`/api/v1/games/${gameId}/next`, {
+        method: 'POST',
+        headers: { Accept: 'application/json' },
+      }),
+    )
   }
 </script>
 
