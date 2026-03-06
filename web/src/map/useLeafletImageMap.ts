ca82ef749d9f890475489a0470b3132de64378e4
marinarj <marinarj@myumanitoba.ca>
2026-03-06 11:01:21 -0600

    Fix Leaflet marker icon path detection in Vite dev
    Leaflet can fail to detect the correct marker icon path when
    running in the Vite dev server
    
    Set L.Icon.Default.imagePath = "" in dev so Leaflet does not try to
    guess the path and instead uses the imported icons

--- web/src/map/useLeafletImageMap.ts
+++ web/src/map/useLeafletImageMap.ts
@@ -6,6 +6,10 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
+if (import.meta.env.DEV) {
+  L.Icon.Default.imagePath = ""
+}
+
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
