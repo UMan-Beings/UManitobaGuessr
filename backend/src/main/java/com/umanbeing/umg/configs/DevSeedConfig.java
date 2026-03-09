e3aacf4a1f0331848b1a6da4e96fc97a6bc5a4df
marinarj <marinarj@myumanitoba.ca>
2026-03-09 14:21:06 -0500

    Correct coords of two locations

--- backend/src/main/java/com/umanbeing/umg/configs/DevSeedConfig.java
+++ backend/src/main/java/com/umanbeing/umg/configs/DevSeedConfig.java
@@ -31,7 +31,7 @@ public class DevSeedConfig {
 
         new Location("", "https://uman-beings.github.io/UMG-Pictures/56jes45xzfdg1.jpeg",new BigDecimal("2813"), new BigDecimal("1143")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/by35nk00kzcg1.jpeg", new BigDecimal("2718"), new BigDecimal("1065")),
-        new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3659.jpg", new BigDecimal("1259"), new BigDecimal("1158")),
+        new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3659.jpg", new BigDecimal("1259"), new BigDecimal("1262")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3660.jpeg", new BigDecimal("1545"), new BigDecimal("1210")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3662.jpg", new BigDecimal("2476"), new BigDecimal("1350")),
 
@@ -39,7 +39,7 @@ public class DevSeedConfig {
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3665.jpg", new BigDecimal("2774"), new BigDecimal("1756")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3670.jpg", new BigDecimal("3237"), new BigDecimal("1684")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3673.jpg", new BigDecimal("3228"), new BigDecimal("1694")),
-        new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3675.jpg", new BigDecimal("2476"), new BigDecimal("1350")),
+        new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3675.jpg", new BigDecimal("2728"), new BigDecimal("1928")),
 
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3675.jpg",new BigDecimal("2719"), new BigDecimal("1942")),
         new Location("", "https://uman-beings.github.io/UMG-Pictures/IMG_3676.jpeg", new BigDecimal("2688"), new BigDecimal("1992")),
