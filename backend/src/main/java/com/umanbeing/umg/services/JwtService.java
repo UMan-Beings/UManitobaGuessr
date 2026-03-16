d195dd382337c849305f0a2234d8049a8fbf33a2
GilgameshACP <kunhao.zhang@outlook.com>
2026-03-16 13:31:21 -0500

    Update constants

--- backend/src/main/java/com/umanbeing/umg/services/JwtService.java
+++ backend/src/main/java/com/umanbeing/umg/services/JwtService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Date;
@@ -16,10 +17,12 @@ public class JwtService {
     @Value("${JWT_SECRET}")
     private String secret;
 
+    private String subjectJWT = "User Details";
+
     public String generateToken(String username) throws
             IllegalArgumentException, JWTCreationException {
         return JWT.create()
-                .withSubject("User Details")
+                .withSubject(subjectJWT)
                 .withClaim("username", username)
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 1000)) // 1 hour expiration
@@ -29,7 +32,7 @@ public class JwtService {
 
     public String validateTokenAndRetrieveSubject(String token) throws JWTVerificationException{
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
-                .withSubject("User Details")
+                .withSubject(subjectJWT)
                 .build();
 
         DecodedJWT jwt = verifier.verify(token);
@@ -47,7 +50,7 @@ public class JwtService {
 
     private Date extractExpiration(String token) throws JWTVerificationException {
         JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
-                .withSubject("User Details")
+                .withSubject(subjectJWT)
                 .build();
 
         DecodedJWT jwt = verifier.verify(token);
