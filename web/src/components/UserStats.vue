15e49e9ac29320fc13c3c2fc5db25268ddbebf4e
marinarj <marinarj@myumanitoba.ca>
2026-03-17 15:53:58 -0500

    Redesign user stats component

--- web/src/components/UserStats.vue
+++ web/src/components/UserStats.vue
@@ -1,13 +1,35 @@
 <template>
-  <v-sheet class="blur rounded-lg pl-6 pr-6 pt-4 pb-4">
-    <dl class="d-flex flex-row ga-8 text-right">
-      <StatItem label="Total Score" :value="totalScore" />
-      <StatItem label="Total Rounds" :value="totalRounds" />
-      <StatItem label="Total Games" :value="totalGames" />
-      <StatItem label="Average Score" :value="formattedAverageScore" />
-      <StatItem label="Average Guess Time" :value="formattedAverageGuessTime" />
-      <StatItem label="Total Guess Time" :value="formattedTotalGuessTime" />
-    </dl>
+  <v-sheet width="100%" class="pt-4 pa-6" rounded="lg">
+    <div class="d-flex align-center justify-space-between mb-4 flex-wrap ga-2">
+      <div>
+        <h4 class="text-h6 font-weight-medium">
+          Your Stats
+        </h4>
+        <p class="text-body-2 text-medium-emphasis">
+          Track your performance across games
+        </p>
+      </div>
+
+      <div class="d-flex ga-2">
+        <v-btn color="primary" variant="text">
+          Log In
+        </v-btn>
+        <v-btn color="primary" variant="tonal">
+          Sign Up
+        </v-btn>
+      </div>
+    </div>
+
+    <v-sheet class="blur rounded-lg pl-6 pr-6 pt-4 pb-4">
+      <dl class="d-flex flex-row ga-8 text-right">
+        <StatItem label="Total Score" :value="totalScore" />
+        <StatItem label="Total Rounds" :value="totalRounds" />
+        <StatItem label="Total Games" :value="totalGames" />
+        <StatItem label="Average Score" :value="formattedAverageScore" />
+        <StatItem label="Average Guess Time" :value="formattedAverageGuessTime" />
+        <StatItem label="Total Guess Time" :value="formattedTotalGuessTime" />
+      </dl>
+    </v-sheet>
   </v-sheet>
 </template>
 
@@ -24,17 +46,17 @@ const props = defineProps<{
 const formattedAverageScore = computed(() => props.averageScore.toFixed(1))
 
 const formattedAverageGuessTime = computed(
-  () => `${props.averageGuessTimeSeconds.toFixed(1)} Seconds`,
+  () => `${props.averageGuessTimeSeconds.toFixed(1)} seconds`,
 )
 
 const formattedTotalGuessTime = computed(
-  () => `${props.totalGuessTimeSeconds} Seconds`,
+  () => `${props.totalGuessTimeSeconds} seconds`,
 )
 </script>
 
 <style scoped>
 .blur {
   backdrop-filter: blur(6px);
-  background: rgba(0,0,0,.4);
+  background: rgba(0,0,0,.1);
 }
 </style>
