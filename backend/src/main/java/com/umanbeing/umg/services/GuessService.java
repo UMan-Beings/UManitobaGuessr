3baaa25c0498ed49c184e5ab43d4975fd4f5a322
Jason Bilinsky <JasonB26@users.noreply.github.com>
2026-03-20 01:52:43 -0500

    Refactored GuessServiceTest.java

--- backend/src/main/java/com/umanbeing/umg/services/GuessService.java
+++ backend/src/main/java/com/umanbeing/umg/services/GuessService.java
@@ -11,14 +11,13 @@ import com.umanbeing.umg.models.Guess;
 import com.umanbeing.umg.models.Location;
 import com.umanbeing.umg.repos.GuessRepo;
 import com.umanbeing.umg.models.Round;
-import java.lang.Math;
 
 @Service
 public class GuessService {
 
-    private static final double FULL_SCORE_DISTANCE = 50;
-    private static final double MAX_DISTANCE = 350;
-    private static final int MAX_SCORE = 1000;
+    public static final double FULL_SCORE_DISTANCE = 50;
+    public static final double MAX_DISTANCE = 350;
+    public static final int MAX_SCORE = 1000;
 
     private final GuessRepo guessRepo;
 
