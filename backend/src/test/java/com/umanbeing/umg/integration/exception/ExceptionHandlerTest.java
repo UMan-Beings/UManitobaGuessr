aaf252329942c1fd2c87bb022f7bcf6824c780ed
GilgameshACP <kunhao.zhang@outlook.com>
2026-03-21 18:06:52 -0500

    Fixed some tests

--- backend/src/test/java/com/umanbeing/umg/integration/exception/ExceptionHandlerTest.java
+++ backend/src/test/java/com/umanbeing/umg/integration/exception/ExceptionHandlerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.access.AccessDeniedException;
 
 
 public class ExceptionHandlerTest extends PostgresIntegrationTestBase{
