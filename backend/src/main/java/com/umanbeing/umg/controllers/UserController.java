e7f5a25c5af58afeef037d3f0654da37720c691d
marinarj <marinarj@myumanitoba.ca>
2026-03-26 13:08:39 -0500

    Use JWT to retrieve personal stats

--- backend/src/main/java/com/umanbeing/umg/controllers/UserController.java
+++ backend/src/main/java/com/umanbeing/umg/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.umanbeing.umg.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.umanbeing.umg.controllers.dto.UserStatsResponse;
+import com.umanbeing.umg.models.User;
 import com.umanbeing.umg.services.GameService;
+import com.umanbeing.umg.services.UserService;
 
 import lombok.AllArgsConstructor;
 
@@ -16,10 +20,26 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserController {
     private final GameService gameService;
+    private final UserService userService;
 
-    @GetMapping("/{userId}/stats")
-    public ResponseEntity<UserStatsResponse> getUserStats(@PathVariable Long userId) {
-        UserStatsResponse response = gameService.getUserStats(userId);
+    @GetMapping("/me/stats")
+    public ResponseEntity<UserStatsResponse> getUserStats(Authentication authentication) {
+        User user = getAuthenticatedUser(authentication);
+        UserStatsResponse response = gameService.getUserStats(user.getUserId());
         return ResponseEntity.ok(response);
+    }
+
+    private User getAuthenticatedUser(Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
+        }
+
+        User user = userService.getUserByEmail(authentication.getName());
+        
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
+        }
+
+        return user;
     }
 }
