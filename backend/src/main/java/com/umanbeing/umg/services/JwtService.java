9a58827311765b68fc732ea46d12df0e5312d4e8
GilgameshACP <kunhao.zhang@outlook.com>
2026-03-31 16:51:38 -0500

    Removing a import leftover

--- backend/src/main/java/com/umanbeing/umg/services/JwtService.java
+++ backend/src/main/java/com/umanbeing/umg/services/JwtService.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import io.jsonwebtoken.ExpiredJwtException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 import org.springframework.beans.factory.annotation.Value;
